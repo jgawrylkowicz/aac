063e81a97fc5c9e1b0c6563b76620443af0aae4e
Jakub Gawrylkowicz <jgawrylkowicz@gmail.com>
2018-04-24 14:04:08 +0200

    refractor board variables and update the grid

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,10 +18,15 @@ export class HomePage {
   native:boolean;
 
   settings:any;
-  boards:Array<string>;
-  grid:Array<number>;
-  // instead of getting a button in the frontend,
-  // the buttons have to be assigned here
+  boards:Array<any>;
+  currentBoard:Array<any>;
+
+  path:string; // path to the unzipped board with all the assets
+
+  // grid and boards need a refractor
+  // grid is used to draw the 2d communication board and contains all the buttons in the right order
+  // boards is an array of communication boards
+  // currentboard needs to be implemented
 
   constructor(public navCtrl: NavController,
     public plt: Platform,
@@ -29,8 +34,8 @@ export class HomePage {
     private zip:Zip,
     public http:Http) {
 
-      this.boards = new Array<string>();
-      this.grid = new Array();
+      this.boards = new Array<any>();
+      this.currentBoard = new Array<any>();
 
   }
 
@@ -48,24 +53,24 @@ export class HomePage {
   async loadBoard(){
 
     this.settings = await this.getBoardSettings();
-    //console.log("settings",this.settings);
-
+    // at the time only for the root board
     let board = await this.getBoard(this.settings.root);
-    console.log("board",board);
-    this.boards.push(board);
-    this.grid = this.getBoardGridOrder(this.boards[0]);
-    console.log("grid",this.grid);
-
-
-
+    let transformedBoard:Array<any> = this.transform(board)
+    this.boards.push(transformedBoard); // adds the transformed board to the array
+    this.setBoardAsActive(0); // sets the root board as the active one
 
   }
+
+
 
   // need an interface for settings later
   private getBoardSettings():Promise<Array<string>>{
 
     let url:string = 'assets/cache/communikate-20/';
     let file:string = 'manifest.json';
+
+    // url is need to get images later on
+    this.path = url;
 
     return new Promise(resolve => {
        this.http.get(url + file)
@@ -75,13 +80,13 @@ export class HomePage {
     });
   }
 
-  getBoard(file:string):Promise<string>{
+  getBoard(filename:string):Promise<string>{
 
     let url:string = 'assets/cache/communikate-20/';
     //check if the file exists
 
     return new Promise(resolve => {
-      this.http.get(url + file)
+      this.http.get(url + filename)
        .map(res =>
          res.json()).subscribe(data => { resolve(data) }
      );
@@ -89,11 +94,27 @@ export class HomePage {
 
   }
 
-  getBoardGridOrder(board){
+  transform(board):Array<any>{
+    let grid = new Array<any>();
     if (board.grid && board.grid.order){
-      return board.grid.order;
+      for (let i = 0; i < board.grid.order.length; i++){
+        grid.push(new Array<any>());
+        for(let j = 0; j < board.grid.order[i].length; j++){
+          let index = board.grid.order[i][j];
+          let button = this.getButtonByID(board,index);
+          button.image_url = this.path + 'images/' + 'image_' + button.image_id + ".png";
+          grid[i].push(button);
+        }
+      }
+      return grid;
     }
+  }
 
+  setBoardAsActive(id:number){
+    if (this.boards && this.boards.length > 0){
+      this.currentBoard = this.boards[id];
+      console.log("currentBoard",this.currentBoard);
+    } else console.log("Error: No boards loaded.")
   }
 
   getButtonByID(board, id:number){
@@ -101,7 +122,7 @@ export class HomePage {
       for (let button of board.buttons){
         if (button.id === id) return button;
       }
-    }
+    } else console.log("Error: This board does not contain any buttons.")
   }
 
   addWord(text: string){
